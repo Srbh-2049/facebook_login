3f597629b200aeed1dd68f7482efc43a874001ac
Srbh-2049 <saurabh.bit2019@gmail.com>
2023-02-06 15:40:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useInitFbSDK } from './fb_init';
 
+let z=""
 function App() {
+  const isFBSDKInitialized=useInitFbSDK()
+  
+  const [imageUrl, setImageUrl] = useState("");
+  const [postCaption, setPostCaption] = useState("");
+  const [isSharingPost, setIsSharingPost] = useState(false);
+  const [token,setToken]=useState("")
+  const [str,changestr]=useState("")
+  
+  const logInToFB=()=>
+  {
+    
+    window.FB.login(function(response) {
+      //console.log(response)
+      
+        setToken(`${response.authResponse.accessToken}`)
+        //z=response.authResponse.accessToken;
+      
+      //console.log(response.authResponse.accessToken)
+      //console.log('State Values: ',token)
+      //console.log(fbUserAcessToken)
+      // handle the response
+    }, {scope: 'public_profile,email'});
+  }
+  console.log(token)
+  const logOutToFB=()=>
+  {
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={logInToFB}>
+        LogIn To Facebook
+      </button>
+     
+      
     </div>
   );
 }
+
+
 
 export default App;
